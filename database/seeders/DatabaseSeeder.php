ad80dd05502af7486e73bb4c750726ac3d495800
Muhamad Haikal Dwi Arya <muhamad.haikal.tif25@polban.ac.id>
2026-09-25 23:48:58 +0700

    Task Final

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // Panggil seeder kegiatan yang sudah kita buat
+        $this->call(ActivitySeeder::class);
     }
 }
